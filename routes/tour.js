0a6eb097122c599a1f01f8da3deb85bbc2f2fa3f
Amey0008 <ameykachare1234@gmail.com>
2023-10-02 13:11:12 +0530

    first commit

---
+++ routes/tour.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createTour,
+    updateTour,
+    deleteTour,
+    getSingleTour,
+    getAllTour,
+    getTourBySearch,
+    getFeaturedTour,
+    getTourCount
+ } from '../controller/tourController.js';
+
+const router=express.Router()
+
+router.post('/',createTour)
+
+router.put('/:id',updateTour)
+
+router.delete('/:id',deleteTour)
+
+router.get('/:id',getSingleTour)
+
+router.get('/',getAllTour)
+
+router.get('/search/getTourBySearch',getTourBySearch)
+
+router.get('/search/getFeaturedTours',getFeaturedTour)
+
+router.get('/search/getTourCount',getTourCount)
+
+export default router; 
---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import tourRoute from "./routes/tour.js";
+
+dotenv.config();
+const app=express();
+const port=process.env.PORT||8000;
+
+// app.get("/",(res,req)=>{
+//     res.send("api is working");
+// })
+
+//connect mongodb
+mongoose.set("strictQuery",false);
+const connect=async()=>{
+    try{
+        await mongoose.connect(process.env.connection_string,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("monhodb Connected");
+    }catch(err){
+        console.log("monhodb Connection fault");
+    }
+}
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use('/tours',tourRoute);
+
+app.listen(port,()=>{
+    connect();
+    console.log("server is listening on port",port);
+})
